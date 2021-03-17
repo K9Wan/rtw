7a4aaf200055bcf398ec823fe29fb425fdca20bf
K9Wan <40694383+K9Wan@users.noreply.github.com>
2021-03-17 23:32:54 +0900

    Add files via upload

---
+++ sort/insertion_sort/insertion_sort3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void print(int *a, int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        printf("%i ", a[i]);
+    }
+    puts("");
+}
+
+void insertion_sort(int *a, int n)
+{
+    int i, j;
+    for(i=1;i<n;i++)
+    {
+        if(a[i-1]<a[i])
+        {
+            continue;   //for less iteration (optional)
+        }
+        int temp = a[i];
+        for(j=0; j<i; j++)
+        {
+            if(a[j]>temp)
+            {
+                memmove(a+j+1, a+j, (i-j)*sizeof(a[0]));
+                a[j] = temp;
+                break;
+            }
+        }
+        /*/
+        for(j=i-1; j>=0 && a[j]>temp; j--);
+        memmove(a+j+2, a+j+1, (i-j-1)*sizeof(a[0]));
+        a[j+1] = temp;
+        for(j=i; j>0 && a[j-1]>temp; j--);
+        memmove(a+j+1, a+j, (i-j)*sizeof(a[0]));
+        a[j] = temp;
+        /*/
+        //print(a,n);
+    }
+}
+
+int main(void)
+{
+    int arr[] = {1,4,35,23,3,6,8,5,6,15,9,0,32,11,98,13,25,27};
+    insertion_sort(arr, sizeof(arr)/sizeof(arr[0]));
+    print(arr, sizeof(arr)/sizeof(arr[0]));
+
+    return 0;
+}
